4f95161c4936181d9eb1ee8d27bb8eed89a543d1
Henry Derbyshire <henry@htderbyshire.com>
2022-04-16 16:19:14 +0100

    added mock data to cassandra

--- src/main/java/com/watermelon/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ src/main/java/com/watermelon/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -2,6 +2,9 @@ package com.watermelon.betterreadsdataloader;
 
 import java.nio.file.Path;
 
+import com.watermelon.betterreadsdataloader.author.Author;
+import com.watermelon.betterreadsdataloader.author.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -10,18 +13,32 @@ import org.springframework.context.annotation.Bean;
 
 import com.watermelon.betterreadsdataloader.connection.DataStaxAstraProperties;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsDataLoaderApplication {
+
+    @Autowired
+    AuthorRepository authorRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
 	}
 
-  @Bean
-  public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
-    Path bundle = astraProperties.getSecureConnectBundle().toPath();
-    return builder -> builder.withCloudSecureConnectBundle(bundle);
-  }
+    @PostConstruct
+    public void start() {
+        Author author = new Author();
+        author.setId("id");
+        author.setName("Name");
+        author.setPersonalName("personalName");
+        authorRepository.save(author);
+    }
+
+    @Bean
+     public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 
 }
